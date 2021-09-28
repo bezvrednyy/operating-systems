f4d1cd1f24346560844407816c50cf3641ba62c3
yakov.algaev <Algaev18@gmail.com>
2021-09-28 16:23:11 +0300

    master Сделал новый парсинг автомата Мура

--- labs/minimization/index.js
+++ labs/minimization/index.js
@@ -1,37 +1,79 @@
 import * as fs from 'fs'
 
-/**
- * @typedef {{
- *   state: string,
- *   outputSignal: string,
- * }}
- */
-let StateInfo
+/** @typedef {string} */
+let State
+
+/** @typedef {string} */
+let OutputSignal
+
+/** @typedef {string} */
+let InputSignal
 
 /**
- * @typedef {Map<string, Array<StateInfo>>}
+ * @typedef {{
+ *   inputSignal: InputSignal,
+ *   endState: State,
+ * }}
  */
-let MoorAutomaton
+let TransitionInfo
+
+/**
+ * @typedef {Map<State, Array<TransitionInfo>>}
+ */
+let MoorStateInfo
+
+/**
+ * @typedef {Map<OutputSignal, MoorStateInfo>}
+ */
+let MoorInitialAutomaton
 
 /**
  * @param {Array<string>} rawData
- * @return {MoorAutomaton}
+ * @return {MoorInitialAutomaton}
  */
 function parseMoorAutomaton(rawData) {
-    /** @type {MoorAutomaton} */
+    /** @type {MoorInitialAutomaton} */
     const moorAutomaton = new Map()
-    const outputSignals = rawData[0].split(' ')
+    /** @type {Map<InputSignal, Array<State>>} */
+    const transitionsMap = new Map()
+
     for (let i = 1; i < rawData.length; ++i) {
         const [inputSignal, rawStates] = rawData[i].split(': ')
         const states = rawStates.split(' ')
-        /** @type {Array<StateInfo>} */
-        const statesInfo = states.map((state, index) => ({
-            state,
-            outputSignal: outputSignals[index],
-        }))
-        moorAutomaton.set(inputSignal, statesInfo)
+        transitionsMap.set(inputSignal, states)
     }
+
+    const moorStates = rawData[0].split(' ')
+    moorStates.forEach((moorState, index) => {
+        const [state, outputSignal] = moorState.split('/')
+        const stateTransitionsMap = moorAutomaton.get(outputSignal)
+        if (stateTransitionsMap) {
+            if (stateTransitionsMap.has(state)) {
+                throw new Error(`Ошибка синтаксиса: дублирование состояния: "${state}".`)
+            }
+            stateTransitionsMap.set(state, convertRawTransitionsToArrayByIndex(transitionsMap, index))
+        }
+        else {
+            moorAutomaton.set(outputSignal, new Map([
+                [state, convertRawTransitionsToArrayByIndex(transitionsMap, index)],
+            ]))
+        }
+    })
+
     return moorAutomaton
+}
+
+/**
+ * @param {Map<InputSignal, Array<State>>} rawTransitions
+ * @param {number} index
+ * @return
+ */
+function convertRawTransitionsToArrayByIndex(rawTransitions, index) {
+    const inputSignals = Array.from(rawTransitions.keys())
+    return inputSignals.map(x => ({
+        inputSignal: x,
+        endState: rawTransitions.get(x)[index],
+    }))
 }
 
 function start() {
@@ -47,20 +89,26 @@ function start() {
         const dataRows =  data.split('\n')
         if (dataRows[0] === 'Mr') {
             const moorAutomaton = parseMoorAutomaton(dataRows.slice(4))
-            printMoorAutomaton(moorAutomaton)
         }
     })
 }
 
-/**
- * @param {MoorAutomaton} automaton
- */
-function printMoorAutomaton(automaton) {
-    automaton.forEach((statesInfo, inputSignal) => {
-        let statesText = ''
-        statesInfo.forEach(x => statesText += ` ${x.state}/${x.outputSignal}`)
-        console.log(`${inputSignal}:${statesText}`)
-    })
-}
+// /**
+//  * @param {MoorAutomaton} automaton
+//  */
+// function printMoorAutomaton(automaton) {
+//     automaton.forEach((statesInfo, inputSignal) => {
+//         let statesText = ''
+//         statesInfo.forEach(x => statesText += ` ${x.state}/${x.outputSignal}`)
+//         console.log(`${inputSignal}:${statesText}`)
+//     })
+// }
+//
+// /**
+//  * @param {MoorAutomaton} automaton
+//  */
+// function firstMoorMinimization(automaton) {
+//
+// }
 
 start()
